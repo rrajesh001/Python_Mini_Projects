a2a055d6d4399ce042a2ef093079747b4533fefe
rrajesh001 <32635374+rrajesh001@users.noreply.github.com>
2021-10-19 12:30:47 +0530

    Update YouTube_Downloader.py

--- YouTube_Downloader.py
+++ YouTube_Downloader.py
@@ -2,24 +2,25 @@ from pytube import YouTube
 from pytube import Playlist
 
 # User inputs
-while(True):
-    print("======================== Menue Options ===================================")
-    options = print("Enter your choice of options provided \n 1. To download the single video \n 2. To download the playlist videos \n 0. Exit")
+while True:
+    print("======================== Menu Options =====================================")
+    options = print("Enter your choice of options provided \n 1. To download the single video \n \
+    2. To download the playlist videos \n 0. Exit")
     option = int(input("Enter the choice :- "))
-    if(option==0):
+    if option==0:
         break
-    elif(option == 1):
+    elif option == 1:
         print("===================== Single video download option ====================")
         url = input("Enter the Video URL : ")
         my_video = YouTube(url)
-        print("================== Video Info ==================")
+        print("================== Video Info =========================================")
         print(my_video.title)
         print("Download in progress.....")
         my_video = my_video.streams.get_highest_resolution() 
         my_video.download("./VideoPlaylist")
         print("Video Downloaded successfully :) ")
     else:
-        print("================== Playlist Download option ==========================")
+        print("================== Playlist Download option ===========================")
         link = input("Enter the url of the playlist : ")
         playlist = Playlist(link)
         #path = input("Enter the file path : ")
